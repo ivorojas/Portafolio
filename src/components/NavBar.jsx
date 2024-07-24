e2d066c22f406a9eece91ba13c95bf4464c8b2e6
Ivo . <ivo.2001@hotmail.com>
2024-07-23 23:39:55 -0300

    Diseño responsive actualizado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,30 +7,27 @@ import "./NavBar.css"
 export default function NavBar() {
   return (
     <div className='nav-container' id="home" >
+
       <nav className='nav'>
+
         <NavHashLink to="#about" className="nav-link" smooth >
           <button className='nav-button'>
           about
           </button>
-    
         </NavHashLink>
+
         <NavHashLink to="#work" className="nav-link" smooth>
         <button className='nav-button'>
         work
         </button>
-        
         </NavHashLink>
      
         <a className='lets_connect' href="mailto:ivo.2001@hotmail.com">
         let's connect!
         </a>
-  
-    {/*   <button id='menu_button'>
-          <FiMenu className='icon_menu' />
-        </button> */}
-     
-  
+
       </nav>
+
     </div>
 
 
